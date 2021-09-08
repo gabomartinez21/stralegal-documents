367c448ac6d3be81252f1df31bc2fa407b265926
Luciano <lucianocg4@gmail.com>
2021-09-07 19:55:19 -0500

    creacion de form de variables

---
+++ src/admin/db/variables_db.php
@@ -0,0 +1,26 @@
+<?php 
+
+class DB{
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
+
+    public function __construct() {
+        $this->host     = 'localhost';
+        $this->db       = 'variables';
+        $this->user     = '';
+        $this->password = "";
+        $this->charset  = '';
+    }
+    
+    function connect() {
+        try{
+
+
+            $connection = "mysql:host=".$this->host.";dbname=" . $this->db .
+        }
+    }
+    
+}
